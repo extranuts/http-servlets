1c84b851bd8b7981a534091c7bd7225b726ac474
extranuts <QroWka8462>
2021-07-21 19:37:47 +0300

    Add get from id change props to yaml and disable stacktrace from error in brouser

--- src/main/java/com/example/springbootess/controller/AnimeController.java
+++ src/main/java/com/example/springbootess/controller/AnimeController.java
@@ -9,8 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -31,6 +33,15 @@ public class AnimeController {
                 dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
 
         return  ResponseEntity.ok(animeRepository.listAll());
+    }
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable int id){
+        Anime animeFound = animeRepository.listAll()
+                .stream()
+                .filter(anime -> anime.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime is not FOUND"));
+        return ResponseEntity.ok(animeFound);
     }
 
 
