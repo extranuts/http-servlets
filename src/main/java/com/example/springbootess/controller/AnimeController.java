a54f18d19b6be7224180992cb1039ff8f3780f5d
extranuts <QroWka8462>
2021-07-21 21:33:17 +0300

    Add REST delete method

--- src/main/java/com/example/springbootess/controller/AnimeController.java
+++ src/main/java/com/example/springbootess/controller/AnimeController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
@@ -42,6 +39,15 @@ public class AnimeController {
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime is not FOUND"));
         return ResponseEntity.ok(animeFound);
+    }
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return ResponseEntity.ok(animeRepository.save(anime));
+    }
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id){
+        animeRepository.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
--- src/main/java/com/example/springbootess/repository/AnimeRepository.java
+++ src/main/java/com/example/springbootess/repository/AnimeRepository.java
@@ -1,18 +1,39 @@
 package com.example.springbootess.repository;
 
 import com.example.springbootess.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Repository
 public class AnimeRepository {
 
-    public List<Anime> listAll(){
-        return List.of(
+    private static List<Anime> animes;
+    static {
+        animes = new ArrayList<>(List.of(
                 new Anime(1,"Унесенные призраками"),
                 new Anime(2,"Ходячий замок")
-        );
+        ));
+
+    }
+    public List<Anime> listAll(){
+        return animes;
+    }
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextInt(4,100000));
+        animes.add(anime);
+        return anime;
+    }
+    public void delete(int id){
+        animes.remove(animes
+                .stream()
+                .filter(anime -> anime.getId() == id)
+                .findFirst()
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found")));
     }
 }
